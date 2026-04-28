51180c52ab190133de5345abd03f87f8a880da5e
Orville Wright <orville.wright@yahoo.com>
2026-04-28 13:30:58 -0700

    debug: data dump

--- ml_yf_nlp_news_engine.py
+++ ml_yf_nlp_news_engine.py
@@ -1014,10 +1014,8 @@ class yfnews_reader:
             
             c4_dict = self.yfn_c4_result[cached_state]
             print ( f"###-debug: C4 c4_dict keys:     {c4_dict.keys()}" ) 
-            print ( f"###-debug: C4 dataset_1 keys:   {dataset_1.keys()}" )
+            print ( f"###-debug: C4 dataset_1:        {dataset_1}" )
             print ( f"###-debug: C4 c4_dict data:     {c4_dict['data']}" )
-            print ( f"###-debug: C4 dataset_1 result: {dataset_1['result']}" )
-            print ( f"###-debug: C4 dataset_1 raw:    {dataset_1['result']}" )
             
             # print the keys of the C4 result dict
             art_all_p = list()                                          # ensure temp list is empty
