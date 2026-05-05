e5aa61327ce243688d0bfeb0114bdcdd5fba840c
orville-wright <orville.wright@yahoo.com>
2026-05-04 18:32:31 -0700

    fix: captured key error in sub-dickt

--- dump_db.py
+++ dump_db.py
@@ -94,17 +94,11 @@ def dump_lmdb_by_key(lmdb_instance, key_filter):
                         print ( f"  Chunk dict: {_v_key} / Chunk id: {_v_sub_dict["chunk"]} / Ticker: {_v_sub_dict["symbol"]}" )
                         print ( f"  N-grams:    {_v_sub_dict["n-grams"]} / Tokens: {_v_sub_dict["tokenz"]} / Alphas: {_v_sub_dict["alphas"]}" )
                         print ( f"  Chunk sentement:    {_v_sub_dict["sent_type"]} / Sentment score: {_v_sub_dict["sent_score"]} / Chunker used: {_v_sub_dict["trct_state"]}" )
-
-                        #  print(f"\n{matches:03} | db_id:{db_id}  ticker:{ticker}  matched_on:{matched_on}")
-                        #print(f"      hash : {url_hash}")
-                        #print(f"Data: {type(value_str)} - {value_str[:80]}{'...' if len(value_str) > 80 else ''}")
-                        #print(f"Data: {type(value_str)} - {value_str}" )
-                        #pprint(value_str, indent=4, width=40)
                         matches += 1
                         _v_key = 0
                         _v_chunk_dict = 0
-                        print (" ")
 
+                print (" ")
             print(f"\nKey filter '{key_filter}': {matches} match(es) from {total} total entries")
     except lmdb.Error as e:
         print(f"LMDB Error: {e}")
