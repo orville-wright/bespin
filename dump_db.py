c4d98aff748b16ab9b633cf29e2c581776f5bf82
orville-wright <orville.wright@yahoo.com>
2026-05-08 14:05:40 -0700

    debug: zstd decoding strangness

--- dump_db.py
+++ dump_db.py
@@ -219,8 +219,9 @@ def dump_lmdb_articles(lmdb_instance, ticker_filter=None, article_limit=None):
                 try:
                     _zstd_article_text = _v_dict["zstd_blob"]  # test if dic has ZSTD compressed article entry
                     print ( f"ZSTD article blob: {_zstd_article_text[:100]}{'...' if len(_zstd_article_text) > 1 else ''}" )
+                    print ( f"type1: ({type(_zstd_article_text)}) / type2: {(_zstd_article_text).decode('utf-8')}" )
                     decompressor = zstd.ZstdDecompressor()
-                    zstd_blob_uncompressed = decompressor.decompress(_zstd_article_text).encode('utf-8')
+                    zstd_blob_uncompressed = decompressor.decompress(_zstd_article_text).decode('utf-8')
                     #= zstd.ZstdDecompressor().decompress(_zstd_article_text).decode('utf-8')
                     print ( f"{zstd_blob_uncompressed}" )                                                        
                     matches += 1
