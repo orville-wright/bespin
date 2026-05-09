9c35f7f7a863d5d1d117a3380512e41433691139
Orville Wright <orville.wright@yahoo.com>
2026-05-08 23:49:16 -0700

    fix: IDFK

--- dump_db.py
+++ dump_db.py
@@ -331,9 +331,14 @@ elif args['bool_init'] is True:
     print ( "Initializing New Empty LMDB KV Database..." )
     lmdb_dbname = "LMDB_0001"
     lmdb_inst.close_lmdb("RO_DUMP")
-    lmdb_inst = lmdb_io_eng(1, lmdb_dbname, args)
+    lmdb_inst.close_lmdb("RO_DUMP")
+    lmdb_inst.close_lmdb("RO_DUMP")
+    lmdb_inst.close_lmdb("RO_DUMP")
+    lmdb_inst.close_lmdb("RO_DUMP")
+            
+    lmdb_inst = lmdb_io_eng(2, lmdb_dbname, args)
     lmdb_inst.open_lmdb_RW("INTI_DUMP")
-    lmdb_inst.drop_lmdb_RW(1, lmdb_inst)
+    lmdb_inst.drop_lmdb_RW(2, lmdb_inst)
 else:
     print ( f"ERROR: No valid dump option selected. Please choose one of the following:" )
     parser.print_help()
