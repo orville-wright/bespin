997d5c769ea25c22966e34d1382a316264ea3743
Orville Wright <orville.wright@yahoo.com>
2026-05-09 12:39:44 -0700

    hacking: trying again...

--- dump_db.py
+++ dump_db.py
@@ -334,12 +334,12 @@ elif args['bool_xray'] is True:
 elif args['bool_init'] is True:
     lmdb_dbname = "LMDB_0001"
     print ( f"Initialize empty LMDB by dropping all data from {lmdb_inst.db_path}{lmdb_dbname}..." )
-    lmdb_inst.lmdb_env.drop_lmdb_RW("INIT_DUMP")
+    lmdb_inst.drop_lmdb_RW("INIT_DUMP")
     #lmdb_inst.RO_env.close() 
-    lmdb_inst.lmdb_env.open_lmdb_RW("INTI_DUMP")
+    lmdb_inst.open_lmdb_RW("INTI_DUMP")
     # I should be able to call .drop_lmdb_RW()
-    with lmdb_inst.RW_env.begin(write=True) as txn:
-        _db_handle = lmdb_inst.RW_env.open_db(name=lmdb_dbname.encode())
+    with lmdb_inst.begin(write=True) as txn:
+        _db_handle = lmdb_inst.open_db(name=lmdb_dbname.encode())
         txn.drop(_db_handle, delete=False)
     lmdb_inst.RW_env.close()
 else:
