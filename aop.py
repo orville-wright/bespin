dc5f98105f398431cfbdb47fc86ee2c5c51691a8
Orville Wright <orville.wright@yahoo.com>
2026-05-14 18:41:12 -0700

    fix: calling wring class method

--- aop.py
+++ aop.py
@@ -640,11 +640,11 @@ def main():
                     match found_sym:
                         # FIX: add unknown elments later (need to gather them from elsewhere first)
                         # Article must be created first, then related to their parent symbol node
-                        case None:
+                        case False:
                             print (f" ")
-                            print ( f"Trigger: {found_sym}: Symbol node [ {news_symbol} ] exist: {type(found_sym)}" )
+                            print ( f"Trigger: {found_sym} {type(found_sym)} - Symbol node [ {news_symbol} ] does NOT exist: " )
                             kg_node_id = kgraphdb.create_sym_node(news_symbol, sentiment_df=sent_ai.sen_df3)
-                            print ( f"Error: Symbol node does NOT exist - creating ! fst:{type(kg_node_id)} / fs:{kg_node_id}" )
+                            print ( f"Creating new Graph symbol node: {type(kg_node_id)} / fs:{kg_node_id}" )
                             _kgec = kgraphdb.create_article_nodes(df_final, news_symbol)
                             kgraphdb.create_sym_art_rels(news_symbol, df_final, agency="Unknown", author="Unknown", published="Unknown", article_teaser="Unknown")
                             created = True
@@ -658,15 +658,15 @@ def main():
                             print ( f"Trigger: {found_sym}: Symbol node [ {news_symbol} ] exist: {type(found_sym)}" )
                             print ( f"Skipping Graph Node creation..." )
                             created = False
-                        case False:
+                        case None:
                             print (f" ")
-                            print ( f"Trigger: {found_sym}: Symbol node [ {news_symbol} ] does NOT exist: {type(found_sym)}" )
+                            print ( f"Trigger: {found_sym} {type(found_sym)} - Empty Symbol node [ {news_symbol} ] discovered: " )
                             _kgec = kgraphdb.create_article_nodes(df_final, news_symbol)
                             kgraphdb.create_sym_art_rels(news_symbol, df_final, agency="Unknown", author="Unknown", published="Unknown", article_teaser="Unknown")
                             created = True
                             #if args['bool_verbose'] is True:
                             print (f" ")
-                            print ( f"Created a new KG node_id: {_kgec}" )
+                            print ( f"Merged new Symbol article node_id: {_kgec}" )
                         case 99:
                             kgraphdb.close_neo4j_auradb("AOP_AURA", kgraphdb.driver)
                         case _:
