c8253d6978442b31f303f8fc5f13604efc1a06db
Orville Wright <orville.wright@yahoo.com>
2026-07-08 17:22:12 -0700

    fix: case typo

--- aop.py
+++ aop.py
@@ -652,7 +652,7 @@ def main():
             print (f"=================== AI NLP Sentiment processing metrics: {news_symbol.upper()} ===================" )
             print (f"LLM Vec Tokenz:  {_ttcz} - Chars: {_tccz} / Words: {_twcz} / scent/paras: {(_tscz + _tpcz + _trcz)} | AI read time: {(ai_sent_time / 60):.2f} mins" )
             print (f"Human read time: {(hpt_mins):.1f} mins ({(hpt_hours):.1f} hours)\t| Human analyst time: {analyst_time:.1f} hours" )
-            print (f"AI performance:  {round((hpt_mins * 60) / (ai_sent_time / 60))} X Faster than a Human\t| HUman analyst cost: ${round(analyst_cost):,}" )
+            print (f"AI performance:  {round((hpt_mins * 60) / (ai_sent_time / 60))} X Faster than a Human\t| Human analyst cost: ${round(analyst_cost):,}" )
             print (" ")
             
             #pd.set_option('display.max_rows', None)
