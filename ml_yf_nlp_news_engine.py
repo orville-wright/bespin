587139b74a0a0033b493ed34875b0c36f74e8eb9
Orville Wright <orville.wright@yahoo.com>
2026-08-01 17:35:25 -0700

    fix: add dict value element wth spaces to heat map dict

--- ml_yf_nlp_news_engine.py
+++ ml_yf_nlp_news_engine.py
@@ -528,8 +528,7 @@ class yfnews_reader:
                 aurl_hash = auh.hexdigest()                     # compute hash
                 if aurl_hash not in dedupe_set:                 # dedupe membership test (deupe_set => set() ) : uniqueness test
                     dedupe_set.add(aurl_hash)                   # add aurl_hash to dupe_set for next membership test
-                    _d = f"{amount:.0f}"
-                    _u = f"{unit_name}"
+                    _d = f"{amount:.0f} {unit_name} ago"
                     self.news_heatmap.update({aurl_hash: _d})      # build unique new age heatmap
                     logging.info( f'{cmi_debug}   - Add unique url hash to ML Ingest DB @ {cg:02}: {aurl_hash[:30]}...' )
                     print(" ")
