80c375069d40321eeaaadebd83020ae3a82f38b0
Orville Wright <orville.wright@yahoo.com>
2026-08-12 19:25:14 -0700

    expr: more dict/lista ccessor testing

--- aop.py
+++ aop.py
@@ -693,7 +693,8 @@ def main():
             for k, v in news_ai.yfn.news_heatmap.items():
                 if i < 3:
                     _ad = v[1]   # agedate dict
-                    print ( f"{k:12.10}  Age: {v[0]:18}{_ad["published_utc"]}\t", end="" )
+                    #print ( f"{k:12.10}  Age: {v[0]:18}{_ad["published_utc"]}\t", end="" )
+                    print ( f"{k:12.10}  Age: {v[0]:18}{_ad["published_utc"]}\t{v[2]}" )
                     i += 1
                     continue
                 else:
