1be7646a6044b1590e5c544fde687ac191d04187
Orville Wright <orville.wright@yahoo.com>
2026-08-23 14:37:06 -0700

    fix: time series back in time controls

--- data_engines_fundamentals/alpaca_md.py
+++ data_engines_fundamentals/alpaca_md.py
@@ -240,7 +240,7 @@ class alpaca_md:
             symbol_or_symbols=[_tkrsym],
             timeframe=TimeFrame.Day,
             start=yesterday,
-            limit=50
+            limit=100
         )
 
         bars = _client.get_stock_bars(prev_close_params)
