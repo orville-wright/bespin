354908503c24e96fae270f0552524a7cee7a2805
Orville Wright <orville.wright@yahoo.com>
2026-08-23 10:21:58 -0700

    debug: dump json packge

--- data_engines_fundamentals/alpaca_md.py
+++ data_engines_fundamentals/alpaca_md.py
@@ -209,7 +209,7 @@ class alpaca_md:
         try:
             data = self._get_json(url, params, f"fetching quote for {symbol.upper()}")
             
-            print (f"#-DEBUG-#212: json_pkg:\n{data}\n")
+            print (f"#-DEBUG-#212: json_pkg:\n{self._get_json}\n")
         
             if 'quotes' in data and symbol.upper() in data['quotes']:
                 quote = data['quotes'][symbol.upper()]
